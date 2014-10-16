f1efaff2765d995d58b5d41f406c8072115bdebc
AlexandreN7 <prxo@live.fr>
2014-10-16 17:44:37 +0200

    Début du code du bras

---
+++ cube_master.X/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: archi
+ *
+ * Created on 16 octobre 2014, 17:36
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
---
+++ cube_master.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=cube_master.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/cube_master.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=cubemaster.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/cubemaster.x.tar
