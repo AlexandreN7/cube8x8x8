134284396b693f0d43e2ca55fcf416348870a724
RobinN7 <robin.beilvert@gmail.com>
2014-10-26 20:47:45 +0100

    .py qui fait clignoter les leds

--- ftdi_PCtoPIC.py
+++ ftdi_PCtoPIC.py
@@ -8,21 +8,29 @@ def main ():
 	with Device (mode = 't') as dev:
 		dev.baudrate = 19200
 	
-		i=0
+		boucle=0
 
-		while i<64 :
-			dev.write(chr(0x00)) 
-			dev.write(chr(0xFF)) 					
-			i=i+1
+		while boucle<7 :
 
-		time.sleep(1.0) 
+			i=0
 
-		i=0
+			while i<64 :
+				dev.write(chr(0x00)) 
+				dev.write(chr(0xFF)) 					
+				i=i+1
 
-		while i<64 :
-		 	dev.write(chr(0xFF))
-		 	dev.write(chr(0x00))
-		 	i=i+1
+			time.sleep(1.0) 
+
+			i=0
+
+			while i<64 :
+		 		dev.write(chr(0xFF))
+		 		dev.write(chr(0x00))
+		 		i=i+1
+
+			time.sleep(1.0) 
+			
+			boucle=boucle+1
 
 #start main
 if __name__ == '__main__' :
