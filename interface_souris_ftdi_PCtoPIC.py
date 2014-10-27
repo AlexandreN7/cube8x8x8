8daeb83f0294cf8fb03d940d065975b8cfb0a996
RobinN7 <robin.beilvert@gmail.com>
2014-10-27 13:08:25 +0100

    joli fichier .py

--- interface_souris_ftdi_PCtoPIC.py
+++ interface_souris_ftdi_PCtoPIC.py
@@ -1,6 +1,20 @@
-# script carre.py
+"""
+script interface_souris_ftdi_PCtoPIC.py
+  ______  _____      _           _
+ |___   /| ___ \    | |         | |
+     / / | |_/ /___ | |__   ___ | |_
+    / /  |    // _ \| '_ \ / _ \|  _|
+   / /   | |\ \ (_) | |_) | (_) | |_
+  /_/    |_| \_\___/|____/ \___/ \__|
+                            7robot.fr
+Cube8x8x8
+                            
+Created by Robin Beilvert
+"""
+
 from tkinter import *
 
+# Matrice pour envoyer les infos de l'interface graphique au ftdi
 matrice_leds = [[0] * 8 for i in range(2)]
 
 # Taille d'un pixel
@@ -12,34 +26,39 @@ def Clic(event):
     # position du pointeur de la souris
     for i in range(8) :
     	if i*pix_size<event.x<=(i+1)*pix_size : 
-            index = i  	
+            # index pour matrice_leds
+            index = i
+            # position horizontale du carré              	
             X = pix_size*i
 
     for i in range(2) :    				
     	if i*pix_size<event.y<=(i+1)*pix_size :
+            # couleur pour matrice_leds            
             couleur=i
+            # position verticale du carré             
             Y = pix_size*i
             
     matrice_leds[couleur][index]=1
 
+    # couleur du carré en fonction de sa position
     if Y==0 :
         color = 'red'
     else :
         color = 'blue'
 
-    # on dessine un carré
+    # on dessine le carré
     cote = pix_size
-    Canevas.create_rectangle(X, Y, X+cote, Y+cote, outline='black',fill=color)
+    Canevas.create_rectangle(X, Y, X+cote-1, Y+cote-1, outline=color, fill=color)
 
 def Envoyer():
-
+    # afficher la matrice dans le terminal
     print (matrice_leds)
 
 
 def Effacer():
-    """ Efface la zone graphique """
+    # effacement de la zone graphique
     Canevas.delete(ALL)
-    """ RAZ de la matrice """ 
+    # RAZ de la matrice
     for i in range(2) :
         for j in range(8) : 
             matrice_leds [i][j]=0
