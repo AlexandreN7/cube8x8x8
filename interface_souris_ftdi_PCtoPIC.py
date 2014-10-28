e6064140bdf766e346d39678e6f147d3e1f692d6
RobinN7 <robin.beilvert@gmail.com>
2014-10-28 01:18:42 +0100

    interface qui marche avec  le ftdi et tout

--- interface_souris_ftdi_PCtoPIC.py
+++ interface_souris_ftdi_PCtoPIC.py
@@ -11,7 +11,7 @@ script interface_souris_ftdi_PCtoPIC.py
                             7robot.fr
 Cube8x8x8
 
-Created by Robin Beilvert
+Created by Robin Beilvert and Alexandre Proux
 """
 
 # Bibliothèques pour ftdi, calculs et gestion du temps
@@ -28,79 +28,87 @@ except ImportError:
     from tkinter import *
 
 
+lignes = 8
+colonnes = 8
+
 # Matrice pour envoyer les infos de l'interface graphique au ftdi
-matrice_leds = [[False] * 8 for i in range(2)]
+matrice_leds = []
+for i in range(lignes):
+    matrice_leds.append([0] * colonnes)
+
 
 # Taille d'un pixel
 pix_size=50
 
+
 def Clic(event):
     """ Gestion de l'événement Clic gauche sur la zone graphique """
 
-    # position du pointeur de la souris
+    # Incrémentation de la matrice en fonction de la position du pointeur de la souris
+    for i in range(8) : 
+    	for j in range(8) :    				
+		    if (i*pix_size<event.x<=(i+1)*pix_size) and (j*pix_size<event.y<=(j+1)*pix_size) :
+		        matrice_leds[j][i] = matrice_leds[j][i]+1 
+
+    # Dessin du carré en fonction de la position et du nombre de clics (couleur)
     for i in range(8) :
-    	if i*pix_size<event.x<=(i+1)*pix_size : 
-            # index pour matrice_leds
-            index = i
-            # position horizontale du carré              	
-            X = pix_size*i
+    		for j in range(8) :    	
 
-    for i in range(2) :    				
-    	if i*pix_size<event.y<=(i+1)*pix_size :
-            # couleur pour matrice_leds            
-            couleur=i
-            # position verticale du carré             
-            Y = pix_size*i
-            
-    matrice_leds[couleur][index]= not matrice_leds[couleur][index]
-
-    # couleur du carré en fonction de sa position
-    if Y==0 :
-        color = 'red'
-    else :
-        color = 'blue'
-
-    # on dessine le carré
-    if matrice_leds[couleur][index]==True :
-        Canevas.create_rectangle(X, Y, X+pix_size-1, Y+pix_size-1, outline=color, fill=color)
-    else :
-        Canevas.create_rectangle(X, Y, X+pix_size-1, Y+pix_size-1, outline='white', fill='white')        
+		    	if  matrice_leds[i][j]%4 == 1:
+		    		Canevas.create_rectangle(j*pix_size, i*pix_size, j*pix_size+pix_size-1,  i*pix_size+pix_size-1, outline='red', fill='red')
+		    	elif matrice_leds[i][j]%4 == 2:
+		    		Canevas.create_rectangle(j*pix_size, i*pix_size, j*pix_size+pix_size-1,  i*pix_size+pix_size-1, outline='blue', fill='blue')
+		    	elif matrice_leds[i][j]%4 == 3:
+		    		Canevas.create_rectangle(j*pix_size, i*pix_size, j*pix_size+pix_size-1,  i*pix_size+pix_size-1, outline='purple', fill='purple')
+		    	else :
+		    		Canevas.create_rectangle(j*pix_size, i*pix_size, j*pix_size+pix_size-1,  i*pix_size+pix_size-1, outline='white', fill='white')
 
 def Envoyer():
     # Afficher la matrice dans le terminal
     print (matrice_leds)
 
     # Formation des octets à envoyer à partir de la matrice
-    octet_rouge=0
-    octet_bleu=0
+    # Les 8 indices des octets correspondent aux 8 PICs
+    octets_rouges=[0,0,0,0,0,0,0,0]
+    octets_bleus=[0,0,0,0,0,0,0,0]
 
     for i in range(8) :
-        octet_rouge = octet_rouge+matrice_leds[0][i]*(2**i) 
-    for i in range(8) :
-        octet_bleu = octet_bleu+matrice_leds[1][i]*(2**i)
+        for j in range(8) :
 
-    print ("Octet rouge = %s" % bin(octet_rouge))
-    print ("Octet bleu = %s" % bin(octet_bleu))
+            if  matrice_leds[i][j]%4 == 1:
+                octets_rouges[i] = octets_rouges[i]+2**j
+
+            elif matrice_leds[i][j]%4 == 2:                
+                octets_bleus[i] = octets_bleus[i]+2**j
+
+            elif matrice_leds[i][j]%4 == 3:
+                octets_rouges[i] = octets_rouges[i]+2**j
+                octets_bleus[i] = octets_bleus[i]+2**j
+
+
+    print ("Premier octet rouge = %s" % bin(octets_rouges[0]))
+    print ("Premier octet bleu = %s" % bin(octets_bleus[0]))
 
     # On envoie la sauce !
     with Device (mode = 't') as dev:
         dev.baudrate = 19200
 
-        i=0
-        while i<64 :
-            dev.write(chr(octet_bleu))
-            dev.write(chr(octet_rouge))                                
-            i=i+1
+        # 8 étages
+        for k in range(8) :
+            # 8 PICs = 8 lignes bicolores
+            for i in range (8) :
+                dev.write(chr(octets_bleus[i]))
+                dev.write(chr(octets_rouges[i]))
 
 
 
 def Effacer():
-    # effacement de la zone graphique
+    # Effacement de la zone graphique
     Canevas.delete(ALL)
     # RAZ de la matrice
-    for i in range(2) :
+    for i in range(8) :
         for j in range(8) : 
-            matrice_leds [i][j]=False
+            matrice_leds [i][j]=0
 
 # Création de la fenêtre principale
 Mafenetre = Tk()
@@ -108,7 +116,7 @@ Mafenetre.title('Carrés')
 
 # Création d'un widget Canvas
 Largeur = 8*pix_size
-Hauteur = 2*pix_size
+Hauteur = 8*pix_size
 Canevas = Canvas(Mafenetre, width = Largeur, height =Hauteur, bg ='white')
 # La méthode bind() permet de lier un événement avec une fonction :
 # un clic gauche sur la zone graphique provoquera l'appel de la fonction utilisateur Clic()
