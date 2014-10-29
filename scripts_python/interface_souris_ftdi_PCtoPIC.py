4678d916f6dd6b7690bbb093cf12d724937ed0b2
RobinN7 <robin.beilvert@gmail.com>
2014-10-29 17:02:28 +0100

    taste that shit bitchZ

--- scripts_python/interface_souris_ftdi_PCtoPIC.py
+++ scripts_python/interface_souris_ftdi_PCtoPIC.py
@@ -41,27 +41,41 @@ for i in range(lignes):
 # Taille d'un pixel du GUI
 pix_size=50
 
+def Change_couleur(i,j):
+	# Incrémentation de la matrice				
+	matrice_leds[i][j] = matrice_leds[i][j]+1 
+	# Changement de couleur du carré  	
+	if  matrice_leds[i][j]%4 == 1 :
+		Canevas.itemconfigure(carre[i][j],fill='red')
+	elif matrice_leds[i][j]%4 == 2 :
+		Canevas.itemconfigure(carre[i][j],fill='blue')
+	elif matrice_leds[i][j]%4 == 3 :
+		Canevas.itemconfigure(carre[i][j],fill='purple')
+	else :
+		Canevas.itemconfigure(carre[i][j],fill='white')
+
 
 def Clic(event):
 	""" Gestion de l'événement Clic gauche sur la zone graphique """
+	for i in range(lignes):
+		for j in range (colonnes):
+			if (j*pix_size<event.x<=(j+1)*pix_size) and (i*pix_size<event.y<=(i+1)*pix_size) :
+				Change_couleur(i,j) 	
 
-	# Incrémentation de la matrice en fonction de la position du pointeur de la souris
-	for i in range(8) : 
-		for j in range(8) :    				
-			if (i*pix_size<event.x<=(i+1)*pix_size) and (j*pix_size<event.y<=(j+1)*pix_size) :
-				matrice_leds[j][i] = matrice_leds[j][i]+1 
 
-	# Dessin du carré en fonction de la position et du nombre de clics (couleur)
-	for i in range(8) :
-		for j in range(8) :    	
-			if  matrice_leds[i][j]%4 == 1 :
-				Canevas.itemconfigure(carre[i][j],fill='red')
-			elif matrice_leds[i][j]%4 == 2 :
-				Canevas.itemconfigure(carre[i][j],fill='blue')
-			elif matrice_leds[i][j]%4 == 3 :
-				Canevas.itemconfigure(carre[i][j],fill='purple')
-			else :
-				Canevas.itemconfigure(carre[i][j],fill='white')
+def Touche(event):
+
+	print (section.get())
+	# Gestion de l'événement Appui sur une touche du clavier
+	touche = event.keysym
+	touche_list=['ampersand','eacute','quotedbl','apostrophe',\
+			     'a','z','e','r',\
+			     'q','s','d','f',\
+			     'w','x','c','v']     
+	for index in range (16):
+		if touche == touche_list[index]:
+			Change_couleur(index//4+4*(section.get()//2),index%4+4*(section.get()%2)) 
+		
 
 def Envoyer():
 	# Formation des octets à envoyer à partir de la matrice
@@ -101,32 +115,14 @@ def Envoyer():
 				dev.write(chr(octets_bleus[i]))
 				dev.write(chr(octets_rouges[i]))
 
-
-
-def Effacer():
+		
+def Init():
 	# Effacement de la zone graphique
 	Canevas.delete(ALL)
-	# RAZ de la matrice
-	for i in range(8) :
-		for j in range(8) : 
-			matrice_leds [i][j]=0
-
-# Création de la fenêtre principale
-Mafenetre = Tk()
-Mafenetre.title('Carrés')
-
-# Création d'un widget Canvas
-Largeur = colonnes*pix_size
-Hauteur = lignes*pix_size
-Canevas = Canvas(Mafenetre, width = Largeur, height =Hauteur, bg ='white')
-# La méthode bind() permet de lier un événement avec une fonction :
-# un clic gauche sur la zone graphique provoquera l'appel de la fonction utilisateur Clic()
-Canevas.bind('<Button-1>', Clic)
-Canevas.pack(padx = 5, pady = 5)
-
-carre = []
-for i in range(lignes):
-	carre.append(\
+	global carre
+	carre = []
+	for i in range(lignes):
+		carre.append(\
 		[Canevas.create_rectangle(0*pix_size, i*pix_size, 0*pix_size+pix_size-1,  i*pix_size+pix_size-1, outline='white', fill='white')]+\
 		[Canevas.create_rectangle(1*pix_size, i*pix_size, 1*pix_size+pix_size-1,  i*pix_size+pix_size-1, outline='white', fill='white')]+\
 		[Canevas.create_rectangle(2*pix_size, i*pix_size, 2*pix_size+pix_size-1,  i*pix_size+pix_size-1, outline='white', fill='white')]+\
@@ -135,12 +131,50 @@ for i in range(lignes):
 		[Canevas.create_rectangle(5*pix_size, i*pix_size, 5*pix_size+pix_size-1,  i*pix_size+pix_size-1, outline='white', fill='white')]+\
 		[Canevas.create_rectangle(6*pix_size, i*pix_size, 6*pix_size+pix_size-1,  i*pix_size+pix_size-1, outline='white', fill='white')]+\
 		[Canevas.create_rectangle(7*pix_size, i*pix_size, 7*pix_size+pix_size-1,  i*pix_size+pix_size-1, outline='white', fill='white')])
+	
+	# RAZ de la matrice
+	for i in range(lignes) :
+		for j in range(colonnes) : 
+			matrice_leds [i][j]=0
+
+# Création de la fenêtre principale
+Mafenetre = Tk()
+Mafenetre.title('Carrés')
+
+# un appui sur une touche du clavier provoquera l'appel de la fonction utilisateur Touche()
+Mafenetre.bind('<Key>', Touche)
+
+# Création d'un widget Canvas
+Largeur = colonnes*pix_size
+Hauteur = lignes*pix_size
+Canevas = Canvas(Mafenetre, width = Largeur, height =Hauteur, bg ='white')
+# La méthode bind() permet de lier un événement avec une fonction :
+# un clic gauche sur la zone graphique provoquera l'appel de la fonction utilisateur Clic()
+Canevas.bind('<Button-1>', Clic)
+
+Canevas.pack(padx = 5, pady = 5)
+
+Init()
+
+Boutons = Canvas(Mafenetre, width = 100, height =100)
+section=IntVar()
+section.set(0)
+bouton1=Radiobutton(Boutons, text="Haut gauche", variable=section, value=0, indicatoron=0, height=3, width=15)
+bouton2=Radiobutton(Boutons, text="Haut droite", variable=section, value=1, indicatoron=0, height=3, width=15)
+bouton3=Radiobutton(Boutons, text="Bas gauche", variable=section, value=2, indicatoron=0, height=3, width=15)
+bouton4=Radiobutton(Boutons, text="Bas droite", variable=section, value=3, indicatoron=0, height=3, width=15)
+bouton1.grid(row=0, column=0)
+bouton2.grid(row=0, column=1)
+bouton3.grid(row=1, column=0)
+bouton4.grid(row=1, column=1)
+
+Boutons.pack(padx = 5, pady = 5)
 
 # Création d'un widget Button (bouton Envoyer)
 Button(Mafenetre, text ='Envoyer', fg="purple", command = Envoyer).pack(side=LEFT, padx = 5, pady = 5)
 
 # Création d'un widget Button (bouton Effacer)
-Button(Mafenetre, text ='Effacer', command = Effacer).pack(side=LEFT, padx = 5, pady = 5)
+Button(Mafenetre, text ='Effacer', command = Init).pack(side=LEFT, padx = 5, pady = 5)
 
 # Création d'un widget Button (bouton Quitter)
 Button(Mafenetre, text ='Quitter', command = Mafenetre.destroy).pack(side=RIGHT, padx = 5, pady = 5)
