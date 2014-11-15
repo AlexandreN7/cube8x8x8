a3dcaedc2e5e2b47a9f229aae6f815908d74bcb9
RobinN7 <robin.beilvert@gmail.com>
2014-11-15 20:25:39 +0100

    pattern alex_cubes

--- scripts_python/ftdi_PCtoPIC.py
+++ scripts_python/ftdi_PCtoPIC.py
@@ -5,67 +5,8 @@ from time import sleep
 from pylibftdi import Device
 
 
-def main ():
-	with Device (mode = 't') as dev:
-		dev.baudrate = 19200
+with Device (mode = 't') as dev:
+	dev.baudrate = 115200
+	dev.write('test')
+		
 	
-		boucle=0
-
-		while boucle<256 :
-
-			i=0
-
-			while i<128 :
-				dev.write(chr(0x00)) 
-				dev.write(chr(boucle)) 					
-				i=i+1
-
-			sleep(0.001)
-
-			i=0
-
-			while i<256 :
-		 		dev.write(chr(0x00))
-		 		i=i+1
-
-			sleep(0.001)			 
-
-			i=0
-
-			while i<128 :
-		 		dev.write(chr(boucle))
-		 		dev.write(chr(0x00))
-		 		i=i+1
-
-			sleep(0.001)
-
-			i=0
-
-			while i<256 :
-		 		dev.write(chr(0x00))
-		 		i=i+1
-
-			sleep(0.001)			
-
-			i=0
-
-			while i<256 :
-		 		dev.write(chr(boucle))
-
-		 		i=i+1
-
-			sleep(0.001)
-
-			i=0
-
-			while i<256 :
-		 		dev.write(chr(0x00))
-		 		i=i+1
-
-			sleep(0.001)						 
-
-			boucle=boucle+1
-
-#start main
-if __name__ == '__main__' :
-	main()
